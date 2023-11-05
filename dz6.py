b6081bc8a01a2b7d4a43603ab99f83900361750b
rtef119 <rtef119@gmail.com>
2023-11-05 14:18:28 +0100

    dz6

--- dz6.py
+++ dz6.py
@@ -14,7 +14,16 @@ def divider(a, b):
        print("Dilutu ne huslovi znakena nemozna!")
        return 0
 
-data = {10: 2, 2: 5, "123": 4, 18: 0, 8: 4}
+"Ctertu "#" ckob pobazutu rezyttat:"
+#data = {10: 2, 2: 5, "123": 4, 18: 0, 8: 4}
+#Dilutu ne huslovi znakena nemozna Nemozna dilutu na 0! [5.0, 0.4, 0, 0, 2.0]
+"abo"
+#data = {10: 2, 2: 5, 123: 4, 18: 0, 8: 4}
+#Nemozna dilutu na 0! [5.0, 0.4, 30.75, 0, 2.0]
+"abo"
+#data = {10: 2, 2: 5, 123: 4, 18: 4}
+#[5.0, 0.4, 30.75, 4.5]
+"vse"
 
 result = []
 for key in data:
