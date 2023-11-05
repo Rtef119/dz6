0db19e796515700475d68ed34625ef8adea7017a
rtef119 <rtef119@gmail.com>
2023-11-05 14:09:38 +0100

    dz6

---
+++ main.py
@@ -0,0 +1,36 @@
+#result = []
+#def divider(a, b):
+    #if a < b:
+        #raise ValueError
+    #if b > 100:
+        #raise IndexError
+    #return a/b
+#data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+#for key in data:
+ #res = divider(key, data[kem])
+ #result.append(res)
+
+#print(result)
+
+
+
+
+#result = []
+#def divider(a, b):
+    #if a < b:
+        #raise TypeError()
+    #else: return
+#raise ValueError()
+    #if b > 100:
+        #print(f"b>100, ")
+    #else:
+        #print(f"b<100")
+#raise IndexError()
+    #return
+#data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+#for key in data:
+ #res = divider(key, data[key])
+ #result.append(res)
+#print(result)
+#print(IndexError)
+#print(ValueError)
